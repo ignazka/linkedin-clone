59f355bc1601b1aa60c2bbb494166775f361f8f0
Philipp Kastl <picastle@posteo.de>
2022-04-23 19:20:13 +0200

    home page left section

--- pages/home.js
+++ pages/home.js
@@ -4,6 +4,7 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import GroupIcon from '@mui/icons-material/Group';
 import OndemandVideoSharpIcon from '@mui/icons-material/OndemandVideoSharp';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 function Home() {
   return (
     <div>
@@ -23,8 +24,32 @@ function Home() {
             <HeaderLink Icon={OndemandVideoSharpIcon} text='Learning' />
             <HeaderLink Icon={BusinessCenterIcon} text='Jobs' />
           </div>
+          <div className='pl-4'>
+            <button className='text-blue-700 font-semibold transition-all hover:border-2 rounded-full border border-blue-700 px-5 py-1.5'>
+              Sign In
+            </button>
+          </div>
         </div>
       </header>
+      <main className='flex flex-col xl:flex-row items-center max-w-screen-lg mx-auto'>
+        <h1 className='text-3xl md:text-5xl text-amber-800/80 max-w-xl !leading-snug pl-4 xl:pl-0'>
+          Welcome to your professional community
+        </h1>
+        <div className='space-y-4'>
+          <div className='intend'>
+            <h2 className='text-xl'>Search for a Job</h2>
+            <ArrowForwardIosRoundedIcon className='text-gray-700' />
+          </div>
+          <div className='intend'>
+            <h2 className='text-xl'>Find a person you know</h2>
+            <ArrowForwardIosRoundedIcon className='text-gray-700' />
+          </div>
+          <div className='intend'>
+            <h2 className='text-xl'>Learn a new skill</h2>
+            <ArrowForwardIosRoundedIcon className='text-gray-700' />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
