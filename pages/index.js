76324711a28aaffe561ec717398ca8ee90ab1fed
Philipp Kastl <picastle@posteo.de>
2022-04-24 15:48:49 +0200

    add session

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import { getSession, useSession } from 'next-auth/react'
 export default function Home() {
+  const { data: session } = useSession()
+  console.log(session)
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -15,10 +18,28 @@ export default function Home() {
           <Sidebar />
           {/* Feed */}
         </div>
-          {/* Widgets */}
+        {/* Widgets */}
       </main>
 
 
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/home',
+      }
+    }
+  }
+
+  return {
+    props: {
+      session
+    }
+  }
 }
